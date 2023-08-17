25e9de7ac46b4f5820bef51e36765796c4671973
Kevin Jolley <jolley427@gmail.comm>
2023-08-17 14:41:31 -0400

    adds search bar in index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   console.log(props);
   return (
     <div id="posts-index">
       <h1>All Posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} alt={post.title} />
-          <p>{post.body}</p>
-          <button onClick={() => props.onShowPost(post)}>More info</button>
-        </div>
-      ))}
+      Search Filter:
+      <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      {props.posts
+        .filter((post) => post.title.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((post) => (
+          <div key={post.id} className="posts">
+            <h2>{post.title}</h2>
+            <img src={post.image} alt={post.title} />
+            <p>{post.body}</p>
+            <button onClick={() => props.onShowPost(post)}>More info</button>
+          </div>
+        ))}
     </div>
   );
 }
